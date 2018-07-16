496c7382ae58d67d355d5bf006476da8c84a96a6
Michael Cormier <u1138933@kingspeak23.wasatch.peaks>
2018-07-16 13:42:34 -0600

    Test bioconda-utils cb3 migration

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -29,7 +29,7 @@ trap rmbuild EXIT
 ## bioconda-utils build (biconda-utils/utils.py get_recipes only supports two levels of nesting. !!Checking if the bicondoa team can change this!!)
 ##  Therefore, in order to test all recieps we have to run each species/build seperately 
 # Homo-sapien
-bioconda-utils build recipes/Homo_sapiens/GRCh37/ config.yaml --force --loglevel debug 
+bioconda-utils build recipes/Homo_sapiens/GRCh37/ config.yaml --loglevel debug 
 #bioconda-utils build recipes/Homo_sapiens/hg19/ config.yaml 
 #bioconda-utils build recipes/Homo_sapiens/hg38-noalt/ config.yaml 
 #bioconda-utils build recipes/Homo_sapiens/hg38/ config.yaml 
